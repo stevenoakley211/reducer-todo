b52b34fd23ba48ee980327692ae439581b66127f
stevenoakley211 <stevenoakley211@live.com>
2020-04-07 22:28:37 -0500

    Initial CRA finished And reducer started

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,31 @@
+export const initialState = [
+    {
+        item:"Add A Task",
+        completed:false,
+        id: Date.now()
+    }
+]
+// The reducer takes in the state object that you want changed and an action call that tells the reducer what change you want made to the state.
+export const todoReducer = (state,action) => {
+    switch(action.type){
+        // If action.type = ADD_ITEM
+        case 'ADD_ITEM' :
+            // Return the a copy of the state with the item added.
+            return [...state,{
+                item: action.payload,
+                completed:false,
+                id:Date.now()
+            }
+        ]
+        // If the action.type = TOGGLE_FINISHED.
+        case 'TOGGLE_FINISHED':
+            // return a copy of the state that has the item (that matches the action.id passed in) with its completed status changed.
+            return state.map(todo => todo.id ===action.id ? {...todo, completed: !todo.completed} :todo)
+            //If the action.type = REMOVE_FINISHED  
+        case 'REMOVE_FINISHED' :
+            // Return a copy of the state that has had tasks that were completed filtered out
+            return state.filter( todo=> !todo.copleted)
+        default:
+            return state;    
+    }
+}
